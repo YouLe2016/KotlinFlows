f5170d848e7c8a6056788af23e56d76b5f5ff98a
江心才子-瑞孚 <270628297@qq.com>
2024-01-12 15:54:00 +0800

    0

--- hilt/src/main/java/com/wyl/hilt/di/AppModule.kt
+++ hilt/src/main/java/com/wyl/hilt/di/AppModule.kt
@@ -7,7 +7,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
 @InstallIn(ActivityComponent::class)
--- hilt/src/main/java/com/wyl/hilt/service/Fruit.kt
+++ hilt/src/main/java/com/wyl/hilt/service/Fruit.kt
@@ -1,8 +1,9 @@
 package com.wyl.hilt.service
 
+import com.wyl.hilt.di.anno.Apple
 import javax.inject.Inject
 
-//class Fruit @Inject constructor(private val name: String) {
+//class Fruit @Inject constructor(@Apple private val name: String) {
 class Fruit(private val name: String) {
     fun name(): String {
         return name
